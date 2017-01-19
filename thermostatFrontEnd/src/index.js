bfac9342be9c6f73b0b7be60363281c5d403a2da
Mathys J Taljaard <mathysjtaljaard@users.noreply.github.com>
2017-01-19 00:39:43 -0500

    Add additional chart for day

--- thermostatFrontEnd/src/index.js
+++ thermostatFrontEnd/src/index.js
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import ThermostatData from "./ThermoStatData";
+import Visualization from "./DataVisualization";
+import VisualizationDay from "./DataVisualizationDay";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
+);
+
+ReactDOM.render(
+    <ThermostatData />,
+    document.getElementById('data')
+);
+
+ReactDOM.render(
+    <Visualization />,
+    document.getElementById('visual')
+);
+
+ReactDOM.render(
+    <VisualizationDay />,
+    document.getElementById('visualDay')
 );
 
