55658780180ea5a387362b8c8d60b9ef4f042c57
Mathys J Taljaard <mathysjtaljaard@users.noreply.github.com>
2017-01-22 17:47:22 -0500

    Updates:
    Created Line Chart Template function, generate new template every call

--- thermostatMonitorBackEnd/src/main/java/org/taljaard/rest/client/controller/ThermostatController.java
+++ thermostatMonitorBackEnd/src/main/java/org/taljaard/rest/client/controller/ThermostatController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.taljaard.model.ThermostatData;
 import org.taljaard.rest.client.services.ThermostatServices;
@@ -27,11 +27,14 @@ public class ThermostatController {
 		return data;
 	}
 	
-	@RequestMapping(method=RequestMethod.GET, value="day/{date}")
-	public List<ThermostatData> getSelectedDaysData(@PathVariable(required=true, name="date") String date) {
+	@RequestMapping(method=RequestMethod.GET, value="range")
+	public List<ThermostatData> getDateRangeValues(
+									@RequestParam("start") String startDate,
+									@RequestParam("end") String endDate	) {
 		
-		DateTime givenDate = new DateTime(date);
-		List<ThermostatData> data = service.getGivensDaysData(givenDate);
+		DateTime start = new DateTime(startDate);
+		DateTime end = new DateTime(endDate);
+		List<ThermostatData> data = service.getDataWithinRange(start, end);
 		
 		return data;
 	}
