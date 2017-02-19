288d8fb20e6fc41c88a266440048d74b623d58d0
Mathys J Taljaard <mathysjtaljaard@users.noreply.github.com>
2017-02-19 11:36:44 -0500

    Update logic to run both webapp and montirong during startup

--- thermostatMonitorBackEnd/src/main/java/org/taljaard/model/ThermostatData.java
+++ thermostatMonitorBackEnd/src/main/java/org/taljaard/model/ThermostatData.java
@@ -6,17 +6,41 @@ import org.joda.time.DateTime;
 
 public class ThermostatData {
 
-	private int id;
-	private String createTimeISO;
-	private Timestamp createTime;
-	private boolean fanOn;
-	private boolean heatOn;
-	private boolean auxHeatOn;
 	private boolean acOn;
-	private double temperature;
+	private int acOnCounter;
+	private Timestamp acOnTime;
+	private boolean auxHeatOn;
+	private int auxHeatOnCounter;
+	private Timestamp auxHeatOnTime;
+	private Timestamp createTime;
+	private String createTimeISO;
+	private boolean fanOn;
+	private int fanOnCounter;
+	private Timestamp fanOnTime;
+	private boolean heatOn;
+	private int heatOnCounter;
+	private Timestamp heatOnTime;
+	private int id;
 	private String rawData;
+	private double temperature;
 	
 	public ThermostatData() {
+	}
+
+	public int getAcOnCounter() {
+		return acOnCounter;
+	}
+
+	public Timestamp getAcOnTime() {
+		return acOnTime;
+	}
+
+	public int getAuxHeatOnCounter() {
+		return auxHeatOnCounter;
+	}
+
+	public Timestamp getAuxHeatOnTime() {
+		return auxHeatOnTime;
 	}
 
 	public Timestamp getCreateTime() {
@@ -27,65 +51,117 @@ public class ThermostatData {
 		return createTimeISO;
 	}
 
-	public void setCreateTime(Timestamp createTime) {
-		this.createTime = createTime;
-		this.createTimeISO = new DateTime(createTime.getTime()).toString();
+	public int getFanOnCounter() {
+		return fanOnCounter;
+	}
+
+	public Timestamp getFanOnTime() {
+		return fanOnTime;
+	}
+
+	public int getHeatOnCounter() {
+		return heatOnCounter;
+	}
+
+	public Timestamp getHeatOnTime() {
+		return heatOnTime;
 	}
 
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public String getRawData() {
+		return rawData;
 	}
 
 	public double getTemperature() {
 		return temperature;
 	}
 
-	public void setTemperature(double temperature) {
-		this.temperature = temperature;
-	}
-
-	public boolean isFanOn() {
-		return fanOn;
-	}
-
-	public void setFanOn(boolean fanOn) {
-		this.fanOn = fanOn;
-	}
-
-	public boolean isHeatOn() {
-		return heatOn;
-	}
-
-	public void setHeatOn(boolean heatOn) {
-		this.heatOn = heatOn;
+	public boolean isAcOn() {
+		return acOn;
 	}
 
 	public boolean isAuxHeatOn() {
 		return auxHeatOn;
 	}
 
-	public void setAuxHeatOn(boolean auxHeatOn) {
-		this.auxHeatOn = auxHeatOn;
+	public boolean isFanOn() {
+		return fanOn;
 	}
 
-	public boolean isAcOn() {
-		return acOn;
+	public boolean isHeatOn() {
+		return heatOn;
 	}
 
 	public void setAcOn(boolean acOn) {
 		this.acOn = acOn;
 	}
-	
-	public String getRawData() {
-		return rawData;
+
+	public void setAcOnCounter(int acOnCounter) {
+		this.acOnCounter = acOnCounter;
 	}
 
+	public void setAcOnTime(Timestamp acOnTime) {
+		this.acOnTime = acOnTime;
+	}
+
+	public void setAuxHeatOn(boolean auxHeatOn) {
+		this.auxHeatOn = auxHeatOn;
+	}
+
+	public void setAuxHeatOnCounter(int auxHeatOnCounter) {
+		this.auxHeatOnCounter = auxHeatOnCounter;
+	}
+
+	public void setAuxHeatOnTime(Timestamp auxHeatOnTime) {
+		this.auxHeatOnTime = auxHeatOnTime;
+	}
+
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+		this.createTimeISO = new DateTime(createTime.getTime()).toString();
+	}
+
+	public void setCreateTimeISO(String createTimeISO) {
+		this.createTimeISO = createTimeISO;
+	}
+
+	public void setFanOn(boolean fanOn) {
+		this.fanOn = fanOn;
+	}
+
+	public void setFanOnCounter(int fanOnCounter) {
+		this.fanOnCounter = fanOnCounter;
+	}
+
+	public void setFanOnTime(Timestamp fanOnTime) {
+		this.fanOnTime = fanOnTime;
+	}
+
+	public void setHeatOn(boolean heatOn) {
+		this.heatOn = heatOn;
+	}
+
+	public void setHeatOnCounter(int heatOnCounter) {
+		this.heatOnCounter = heatOnCounter;
+	}
+
+	public void setHeatOnTime(Timestamp heatOnTime) {
+		this.heatOnTime = heatOnTime;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public void setRawData(String rawData) {
 		this.rawData = rawData;
+	}
+
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
 	}
 
 	public String toString() {
